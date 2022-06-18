f7a5b269dd25384e455bdb841525e07e6608e2b4
moonayyur <jm_kias@esi.dz>
2022-06-18 14:38:00 +0100

    update comments

--- components/AvisPlace.js
+++ components/AvisPlace.js
@@ -4,7 +4,22 @@ import { View, Text, ScrollView, TextInput, Pressable } from "react-native";
 import { Icon } from "@rneui/themed";
 
 export default function AvisPlace() {
-  const [comments, setComments] = useState("");
+  const [comments, setComments] = useState([
+    {
+      user: "Tiziri Batenjal",
+      date: "Il y a 2 mois",
+      text: "C'est l'endroit parfait pour les sorties en famille ! Je recommande.",
+      like: 5,
+      dislike: 0,
+    },
+    {
+      user: "Serina",
+      date: "A l'instant",
+      text: "Je suis d'accord avec Mme Batenjal",
+      like: 0,
+      dislike: 0,
+    },
+  ]);
   const [comment, setComment] = useState("");
   const [rate, setRate] = useState(0);
   const [star, setStar] = useState([
@@ -19,42 +34,45 @@ export default function AvisPlace() {
       </Text>
       {/* comments */}
       <ScrollView className="mx-4">
-        <View className="bg-violet-50 mx-2 rounded-xl mt-4 px-5 py-3">
-          <View className="flex flex-row items-center justify-between pb-2">
-            <Text className="text-base">Tiziri Batenjal</Text>
-            <Text className="text-sm text-right text-violet-400">
-              Il y a 2 mois
-            </Text>
-          </View>
-          <Text className="text-gray-500">
-            C'est l'endroit parfait pour les sorties en famille ! Je recommande.
-          </Text>
-          {/* icons */}
-          <View className="flex flex-row space-x-8">
-            <View className="flex flex-row items-end gap-x-1 pt-4">
-              <Icon
-                name="thumbs-up"
-                type="feather"
-                size={20}
-                color="rgb(107, 114, 128)"
-              />
-              <Text className="text-gray-500">5</Text>
+        {comments.map((comment, key) => (
+          <View
+            key={key}
+            className="bg-violet-50 mx-2 rounded-xl mt-4 px-5 py-3"
+          >
+            <View className="flex flex-row items-center justify-between pb-2">
+              <Text className="text-base">{comment.user}</Text>
+              <Text className="text-sm text-right text-violet-400">
+                {comment.date}
+              </Text>
             </View>
-            <View className="flex flex-row items-end gap-x-1 pt-4">
-              <Icon
-                name="thumbs-down"
-                type="feather"
-                size={20}
-                color="rgb(107, 114, 128)"
-                style={{ transform: [{ scaleX: -1 }] }}
-              />
-              <Text className="text-gray-500">0</Text>
+            <Text className="text-gray-500">{comment.text}</Text>
+            {/* icons */}
+            <View className="flex flex-row space-x-8">
+              <View className="flex flex-row items-end gap-x-1 pt-4">
+                <Icon
+                  name="thumbs-up"
+                  type="feather"
+                  size={20}
+                  color="rgb(107, 114, 128)"
+                />
+                <Text className="text-gray-500">{comment.like}</Text>
+              </View>
+              <View className="flex flex-row items-end gap-x-1 pt-4">
+                <Icon
+                  name="thumbs-down"
+                  type="feather"
+                  size={20}
+                  color="rgb(107, 114, 128)"
+                  style={{ transform: [{ scaleX: -1 }] }}
+                />
+                <Text className="text-gray-500">{comment.dislike}</Text>
+              </View>
             </View>
           </View>
-        </View>
+        ))}
       </ScrollView>
       <View className="border-t px-8 py-5">
-        <View className="flex flex-row justify-between">
+        <View className="flex flex-row justify-between mx-4">
           <Text className="text-gray-500">Donner une note:</Text>
           <View className="flex flex-row space-x-3">
             <Pressable
@@ -83,29 +101,48 @@ export default function AvisPlace() {
                 style={{ opacity: 0.4 }}
               />
             </Pressable>
-            <Icon
-              name="star"
-              type="feather"
-              size={20}
-              color="rgb(129,104,221)"
-              style={{ opacity: 0.4 }}
-            />
-            <Icon
-              name="star"
-              type="feather"
-              size={20}
-              color="rgb(129,104,221)"
-              style={{ opacity: 0.4 }}
-            />
-            <Icon
-              name="star"
-              type="feather"
-              size={20}
-              color="rgb(129,104,221)"
-              style={{ opacity: 0.4 }}
-            />
+            <Pressable
+              onPress={() => {
+                setRate(1);
+              }}
+            >
+              <Icon
+                name="star"
+                type="feather"
+                size={20}
+                color="rgb(129,104,221)"
+                style={{ opacity: 0.4 }}
+              />
+            </Pressable>
+            <Pressable
+              onPress={() => {
+                setRate(1);
+              }}
+            >
+              <Icon
+                name="star"
+                type="feather"
+                size={20}
+                color="rgb(129,104,221)"
+                style={{ opacity: 0.4 }}
+              />
+            </Pressable>
+            <Pressable
+              onPress={() => {
+                setRate(1);
+              }}
+            >
+              <Icon
+                name="star"
+                type="feather"
+                size={20}
+                color="rgb(129,104,221)"
+                style={{ opacity: 0.4 }}
+              />
+            </Pressable>
           </View>
         </View>
+        {/* new comment view */}
         <View className="bg-violet-50 h-12 flex flex-row justify-between items-center rounded-3xl mt-4 px-4">
           <TextInput
             placeholder="Laisser un commentaire..."
@@ -115,7 +152,17 @@ export default function AvisPlace() {
           ></TextInput>
           <Pressable
             onPress={() => {
-              setComments((comments) => [...comments, comment]);
+              setComment(comment);
+              setComments((comments) => [
+                ...comments,
+                {
+                  user: "Unkown",
+                  date: "A l'instant",
+                  text: comment,
+                  like: 0,
+                  dislike: 0,
+                },
+              ]);
               setComment("");
             }}
           >

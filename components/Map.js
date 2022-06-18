e81c788f34f30f053218b5596411aabcf24057b6
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-06-18 15:37:09 +0100

    fix map

--- components/Map.js
+++ components/Map.js
@@ -1,5 +1,6 @@
 import MapView, { Marker, Geojson } from "react-native-maps";
 import Icon from "react-native-ico";
+import { useState } from "react";
 // import alg from "./algeria.json";
 import alger from "./alger.json";
 import tamanrasset from "./tamanrasset.json";
@@ -7,20 +8,30 @@ const algeria = {
   latitude: 28.0339,
   longitude: 1.6596,
   latitudeDelta: 0,
-  longitudeDelta: 21,
+  longitudeDelta: 22,
 };
 export default function Map({ navigation }) {
+  const [geo, setGeo] = useState(true);
+  const [region, setRegion] = useState(algeria);
   return (
     <MapView
-      tabIndex={0}
       followUserLocation={true}
-      region={algeria}
+      region={region}
       showsUserLocation={true}
       style={{
         width: "100%",
         height: "100%",
       }}
       zoomEnabled={true}
+      onRegionChangeComplete={(region) => {
+        if (geo && region.longitudeDelta < 2) {
+          setRegion(region);
+          setGeo(false);
+        } else if (!geo && region.longitudeDelta > 2) {
+          setRegion(region);
+          setGeo(true);
+        }
+      }}
     >
       <Marker
         coordinate={{
@@ -30,17 +41,18 @@ export default function Map({ navigation }) {
         pinColor="#342861"
         onPress={() => navigation.navigate("Place")}
       >
-        <Icon
-          name="map-marker"
-          group="font-awesome"
-          style={{ width: 30, height: 30 }}
-        />
+        {}
+        <Icon name="map-marker" group="font-awesome" width="30" height="30" />
       </Marker>
-      <Geojson geojson={alger} strokeColor="#8168DD" fillColor="#8168DD" />
+      <Geojson
+        geojson={alger}
+        strokeColor="#8168DD"
+        fillColor={geo ? "#8168DD" : "#0000"}
+      />
       <Geojson
         geojson={tamanrasset}
         strokeColor="#8168DD"
-        fillColor="#8168DD"
+        fillColor={geo ? "#8168DD" : "#0000"}
       />
     </MapView>
   );

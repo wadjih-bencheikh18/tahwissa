741e594df51e238c28d08522677809688de72a2c
moonayyur <jm_kias@esi.dz>
2022-06-18 04:01:25 +0100

    minor fixes

--- components/AvisPlace.js
+++ components/AvisPlace.js
@@ -1,19 +1,17 @@
 import React, { useState } from "react";
 
-import {
-  View,
-  Text,
-  ScrollView,
-  Image,
-  TextInput,
-  Pressable,
-} from "react-native";
+import { View, Text, ScrollView, TextInput, Pressable } from "react-native";
 import { Icon } from "@rneui/themed";
-import mkm2 from "../images/makam2.png";
 
 export default function AvisPlace() {
   const [comments, setComments] = useState("");
   const [comment, setComment] = useState("");
+  const [rate, setRate] = useState(0);
+  const [star, setStar] = useState([
+    { type: "feather", opac: 0.4 },
+    { type: "FontAwesome", opac: 0.9 },
+  ]);
+
   return (
     <View className="h-full">
       <Text className="p-20 border-b border-violet-400">
@@ -23,12 +21,14 @@ export default function AvisPlace() {
       <ScrollView className="mx-4">
         <View className="bg-violet-50 mx-2 rounded-xl mt-4 px-5 py-3">
           <View className="flex flex-row items-center justify-between pb-2">
-            <Text className="text-base">Moh Babeloued</Text>
+            <Text className="text-base">Tiziri Batenjal</Text>
             <Text className="text-sm text-right text-violet-400">
               Il y a 2 mois
             </Text>
           </View>
-          <Text className="text-gray-500">plasa 1tik khawti</Text>
+          <Text className="text-gray-500">
+            C'est l'endroit parfait pour les sorties en famille ! Je recommande.
+          </Text>
           {/* icons */}
           <View className="flex flex-row space-x-8">
             <View className="flex flex-row items-end gap-x-1 pt-4">
@@ -48,38 +48,7 @@ export default function AvisPlace() {
                 color="rgb(107, 114, 128)"
                 style={{ transform: [{ scaleX: -1 }] }}
               />
-              <Text className="text-gray-500">5</Text>
-            </View>
-          </View>
-        </View>
-        <View className="bg-violet-50 mx-2 rounded-xl mt-4 px-5 py-3">
-          <View className="flex flex-row items-center justify-between pb-2">
-            <Text className="text-base">Moh Babeloued</Text>
-            <Text className="text-sm text-right text-violet-400">
-              Il y a 2 mois
-            </Text>
-          </View>
-          <Text className="text-gray-500">plasa 1tik khawti</Text>
-          {/* icons */}
-          <View className="flex flex-row space-x-8">
-            <View className="flex flex-row items-end gap-x-1 pt-4">
-              <Icon
-                name="thumbs-up"
-                type="feather"
-                size={20}
-                color="rgb(107, 114, 128)"
-              />
-              <Text className="text-gray-500">5</Text>
-            </View>
-            <View className="flex flex-row items-end gap-x-1 pt-4">
-              <Icon
-                name="thumbs-down"
-                type="feather"
-                size={20}
-                color="rgb(107, 114, 128)"
-                style={{ transform: [{ scaleX: -1 }] }}
-              />
-              <Text className="text-gray-500">5</Text>
+              <Text className="text-gray-500">0</Text>
             </View>
           </View>
         </View>
@@ -88,20 +57,32 @@ export default function AvisPlace() {
         <View className="flex flex-row justify-between">
           <Text className="text-gray-500">Donner une note:</Text>
           <View className="flex flex-row space-x-3">
-            <Icon
-              name="star"
-              type="feather"
-              size={20}
-              color="rgb(129,104,221)"
-              style={{ opacity: 0.4 }}
-            />
-            <Icon
-              name="star"
-              type="feather"
-              size={20}
-              color="rgb(129,104,221)"
-              style={{ opacity: 0.4 }}
-            />
+            <Pressable
+              onPress={() => {
+                setRate(1);
+              }}
+            >
+              <Icon
+                name="star"
+                type="FontAwesome"
+                size={20}
+                color="rgb(129,104,221)"
+                style={{ opacity: 0.9 }}
+              />
+            </Pressable>
+            <Pressable
+              onPress={() => {
+                setRate(2);
+              }}
+            >
+              <Icon
+                name="star"
+                type="feather"
+                size={20}
+                color="rgb(129,104,221)"
+                style={{ opacity: 0.4 }}
+              />
+            </Pressable>
             <Icon
               name="star"
               type="feather"
